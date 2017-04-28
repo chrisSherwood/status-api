079b9ba6b7391db659cc5796c6c9aad7a44c0c83
chrisSherwood <c.a.sherwood@swansea.ac.uk>
2017-04-28 13:18:00 +0100

    first commit testing ngprime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'BOOM it works!';
+  message:string;
+  name:string;
+  clicked:boolean = false;
+
+  onclick() {
+    this.message = 'You typed ' + this.name +  ' & clicked';
+    this.clicked = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { InputTextModule, ButtonModule, SharedModule, PanelModule } from 'primeng/primeng'; 
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ButtonModule,
+    SharedModule,
+    PanelModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
